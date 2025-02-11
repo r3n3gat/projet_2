c8ca3f86e6bc101a123e6832b9770c993ff5dfa9
r3n3gat <stevi.enoto@gmail.com>
2025-02-11 10:20:06 +0100

    modif scrapper.py : supp os and add urljoin

--- method3/scraper.py
+++ method3/scraper.py
@@ -1,7 +1,7 @@
 # method2/scraper.py
 import requests
 from bs4 import BeautifulSoup
-import os
+from urllib.parse import urljoin
 
 
 def get_category_links(base_url):
@@ -15,7 +15,7 @@ def get_category_links(base_url):
         for category in categories:
             name = category.text.strip()
             relative_link = category['href']
-            full_link = os.path.join(base_url, relative_link)
+            full_link = urljoin(base_url, relative_link)  # Correction ici
             category_links[name] = full_link
 
         return category_links
@@ -33,18 +33,18 @@ def get_books_links(category_url):
             soup = BeautifulSoup(response.content, 'html.parser')
             books = soup.find_all('h3')
             for book in books:
-                link = book.find('a')['href'].replace('../../../', 'https://books.toscrape.com/catalogue/')
-                book_links.append(link)
+                relative_link = book.find('a')['href']
+                book_url = urljoin(category_url, relative_link)  # Correction ici
+                book_links.append(book_url)
 
             next_button = soup.find('li', class_='next')
             if next_button:
                 next_page = next_button.find('a')['href']
-                category_url = os.path.join(os.path.dirname(category_url), next_page)
+                category_url = urljoin(category_url, next_page)  # Correction ici
             else:
                 category_url = None
         else:
-            print(
-                f"[ERREUR] Échec de la récupération des livres pour {category_url} (Status Code: {response.status_code}).")
+            print(f"[ERREUR] Échec de la récupération des livres pour {category_url} (Status Code: {response.status_code}).")
             break
 
     return book_links
